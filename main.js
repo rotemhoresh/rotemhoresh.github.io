5ca634a1467f26f5722e47f602d3669d4c3ed10e
rotemhoresh <horesh.rotem@gmail.com>
2025-01-15 22:20:18 +0200

    feat: theme toggle

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const cursor = document.getElementById("cursor");
 setInterval(() => {
   cursor.classList.toggle("disappear");
 }, 1000);
+
+const theme = document.getElementById("theme");
+
+theme.addEventListener("click", () => document.body.classList.toggle("dark"));
+
+document
+  .querySelectorAll("ul > li")
+  .forEach((e) => e.addEventListener("click", () => alert("WIP")));
